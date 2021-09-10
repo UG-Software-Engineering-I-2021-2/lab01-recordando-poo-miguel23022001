f713cb64149e63f9e42278a9ea70b8e72acd1f91
Miguel Angel Alvarado Ramos <miguel.alvarado@utec.edu.pe>
2021-09-09 22:39:21 -0500

    .

--- Laboratorio1/src/test/java/EjemploVehiculoTest.java
+++ Laboratorio1/src/test/java/EjemploVehiculoTest.java
@@ -15,4 +15,44 @@ public class EjemploVehiculoTest {
         Automovil toyota = new Automovil(10, 100, 0.9);
         Assert.assertEquals(toyota.viaje(4), "Vehiculo viajo 4.0 km y aun tiene 6,40 de combustible");
     }
+    @Test
+    public void test03(){
+        Camion toyota = new Camion(10, 100, 1.6);
+        Assert.assertEquals(toyota.viaje(4), "Vehiculo viajo 4.0 km y aun tiene 3,60 de combustible");
+    }
+    @Test
+    public void test04(){
+        Automovil toyota = new Automovil(10, 100, 0.9);
+        Assert.assertEquals(toyota.recargar(99), "No  se  puede  reabastecer  el  tanque,  esta  lleno");
+    }
+    @Test
+    public void test05(){
+        Automovil toyota = new Automovil(10, 100, 0.9);
+        Assert.assertEquals(toyota.recargar(3.6), "La cantidad de combustible del vehiculo es: 13.6");
+    }
+    @Test
+    public void test06(){
+        Automovil toyota = new Automovil(10, 100, 0.9);
+        Assert.assertEquals(toyota.viaje(12), "Vehiculo necesita reabastecimiento de combustible");
+    }
+    @Test
+    public void test07(){
+        Camion toyota = new Camion(100, 100, 1.6);
+        Assert.assertEquals(toyota.recargar(5), "No  se  puede  reabastecer  el  tanque,  esta  lleno");
+    }
+    @Test
+    public void test08(){
+        Camion toyota = new Camion(25, 100, 1.6);
+        Assert.assertEquals(toyota.recargar(30), "La cantidad de combustible del vehiculo es: 53.5");
+    }
+    @Test
+    public void test09(){
+        Automovil toyota = new Automovil(100, 100, 0.9);
+        Assert.assertEquals(toyota.recargar(12), "No  se  puede  reabastecer  el  tanque,  esta  lleno");
+    }
+    @Test
+    public void test10(){
+        Camion toyota = new Camion(10, 100, 1.6);
+        Assert.assertEquals(toyota.viaje(30), "Vehiculo necesita reabastecimiento de combustible");
+    }
 }
