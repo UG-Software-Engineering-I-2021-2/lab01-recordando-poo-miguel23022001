4032ecaccbb0a67678d6961505e6a255288756f9
Miguel Angel Alvarado Ramos <miguel.alvarado@utec.edu.pe>
2021-09-11 02:34:18 -0500

    .

--- Laboratorio1/src/test/java/EjemploVehiculoTest.java
+++ Laboratorio1/src/test/java/EjemploVehiculoTest.java
@@ -6,7 +6,7 @@ import camion.Camion;
 
 public class EjemploVehiculoTest {
 
-    // Recarga
+    // recarga
     @Test
     public void test01(){
         Automovil toyota = new Automovil(10, 100, 0.9);
@@ -32,32 +32,62 @@ public class EjemploVehiculoTest {
         Automovil toyota = new Automovil(100, 100, 0.9);
         Assert.assertEquals(toyota.recargar(12), "No  se  puede  reabastecer  el  tanque,  esta  lleno");
     }
-
-    //viaje
-   @Test()
-    public void test06() {
-
-        Automovil toyota = new Automovil(10, 100, 0.9);
-        Assert.assertEquals(toyota.viaje(12), "Vehiculo necesita reabastecimiento de combustible");
+    @Test
+    public void test06(){
+        Automovil toyota = new Automovil(10, 10, 0.5);
+        Assert.assertEquals(toyota.recargar(12), "No  se  puede  reabastecer  el  tanque,  esta  lleno");
     }
     @Test
     public void test07(){
-        Automovil toyota = new Automovil(10, 100, 0.9);
-        Assert.assertEquals(toyota.viaje(4), "Vehiculo viajo 4.0 km y aun tiene 6,40 de combustible");
+        Automovil toyota = new Automovil(25, 60, 0.7);
+        Assert.assertEquals(toyota.recargar(12), "La cantidad de combustible del vehiculo es: 37.0");
     }
-    @Test
-    public void test08(){
-        Camion toyota = new Camion(10, 100, 1.6);
-        Assert.assertEquals(toyota.viaje(4), "Vehiculo viajo 4.0 km y aun tiene 3,60 de combustible");
+
+    //viaje
+   @Test()
+    public void test08() {
+
+        Automovil toyota = new Automovil(10, 100, 0.9);
+        Assert.assertEquals(toyota.viaje(12), "Vehiculo necesita reabastecimiento de combustible");
     }
     @Test
     public void test09(){
         Automovil toyota = new Automovil(10, 100, 0.9);
-        Assert.assertEquals(toyota.viaje(12), "Vehiculo necesita reabastecimiento de combustible");
+        Assert.assertEquals(toyota.viaje(4), "Vehiculo viajo 4.0 km y aun tiene 6,40 de combustible");
     }
     @Test
     public void test10(){
         Camion toyota = new Camion(10, 100, 1.6);
+        Assert.assertEquals(toyota.viaje(4), "Vehiculo viajo 4.0 km y aun tiene 3,60 de combustible");
+    }
+    @Test
+    public void test11(){
+        Automovil toyota = new Automovil(10, 100, 0.9);
+        Assert.assertEquals(toyota.viaje(12), "Vehiculo necesita reabastecimiento de combustible");
+    }
+    @Test
+    public void test12(){
+        Camion toyota = new Camion(10, 100, 1.6);
         Assert.assertEquals(toyota.viaje(30), "Vehiculo necesita reabastecimiento de combustible");
+    }
+
+    //setters y getters
+    @Test
+    public void test13(){
+        Camion toyota = new Camion(10, 100, 1.6);
+        toyota.setcapacidadTanque(13.4);
+        Assert.assertEquals(toyota.getcapacidadTanque(),13.4);
+    }
+    @Test
+    public void test14(){
+        Camion toyota = new Camion(10, 100, 1.6);
+        toyota.setConsumo(3.4);
+        Assert.assertEquals(toyota.getConsumo(),3.4);
+    }
+    @Test
+    public void test15(){
+        Camion toyota = new Camion(10, 100, 1.6);
+        toyota.setCombustible(13.4);
+        Assert.assertEquals(toyota.getCombustible(),13.4);
     }
 }
